9b6deb217062aace8e092baa8d75044194aa0059
RUDRA DILIP JAISWAR <rudrajaiswar19@gmail.com>
2025-09-17 00:17:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      sec.classList.add("show");
+    }
+  });
+});
